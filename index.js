3246929f9b89704055686226809ba95690b869de
Helen <nikishina.olena@gmail.com>
2018-01-08 16:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function logShout(string){
 function sayHiToGrandma(string) {
   return string.toLowercase ,"I can't hear you"
 }
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false

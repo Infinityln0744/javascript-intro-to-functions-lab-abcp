296ab9fceeb3489e70959b59af5e67a4b3a9ac8d
Helen <nikishina.olena@gmail.com>
2018-01-08 16:20:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ function sayHiToGrandma(string) {
 }
 var uppercase = "HELLO!"
  
-uppercase.toUpperCase() === uppercase // true
+uppercase.toUpperCase() === uppercase
  
 var lowercase = 'hello!'
  
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false

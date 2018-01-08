531375be11d7ad517a078686942002b06123f8de
Helen <nikishina.olena@gmail.com>
2018-01-08 17:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,20 +10,8 @@ function shout(string){
  function logWhisper(string){
      console.log(string.toLowerCase());
  }
-function shout(string){
-  return string.toUpperCase()
- }
- function whisper(string){
-   return string.toLowerCase()
- }
- function logShout(string){
-   console.log(string.toUpperCase());
-     }
- function logWhisper(string){
-     console.log(string.toLowerCase());
- }
 function sayHiToGrandma(string) {
-if (string.toLowerCase() === string) {
+if (string.toLowerCase() === string)
  return "I can't hear you!"
  }
 if (string.toUpperCase() === string) {
@@ -32,4 +20,3 @@ if (string.toUpperCase() === string) {
  if (string === "I love you, Grandma.") {
  return "I love you, too."
  }
- +

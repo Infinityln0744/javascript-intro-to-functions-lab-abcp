a293c16f4c6d85e798e69243ccec3e1e1da4fecc
Helen <nikishina.olena@gmail.com>
2018-01-08 17:16:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ function shout(string){
  function logWhisper(string){
      console.log(string.toLowerCase());
  }
+function shout(string){
+  return string.toUpperCase()
+ }
+ function whisper(string){
+   return string.toLowerCase()
+ }
+ function logShout(string){
+   console.log(string.toUpperCase());
+     }
+ function logWhisper(string){
+     console.log(string.toLowerCase());
+ }
  function sayHiToGrandma(string){
    if (string === string. toUpperCase())
    return "YES INDEED"

324f564207c0b4b58b6db5cd90ea5571be615bc5
Helen <nikishina.olena@gmail.com>
2018-01-08 16:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function shout(string) {
   return string.toUpperCase()
 }
 function whisper(string) {
-  return string
+  return string. toLowercase()
 }
-function sayHiToGrandma(string) {
-  return string.toUpperCase()
+function logShout(string){
+  console.log (string.toUpperCase)
+}
 }
 function sayHiToGrandma(string) {
   return string.toLowercase ,"I can't hear you"

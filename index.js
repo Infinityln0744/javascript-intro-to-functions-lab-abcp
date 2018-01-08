d3469a41f3cff180cbf573820ae783e56def1a73
Helen <nikishina.olena@gmail.com>
2018-01-08 16:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ function shout(string) {
    console.log(string.toLowerCase())
  }
  
- function sayHiToGrandma(string) {
+ function sayHiToGrandma(string) 
    if (string.toLowerCase() === string) 
-   }
    {  return "I can't hear you!" else if (string.toUpperCase() === string)
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
